a052aa0d8e2efd62b6a8943d55a00c5a5ce4e89c
Sofiia Moroz <sofiiamoroz@192.168.68.103>
2023-10-12 17:03:16 +0200

    structure folders and clean up

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 class App extends React.Component {
@@ -24,13 +24,13 @@ class App extends React.Component {
   }
   
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchField);
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField);
     });
-    if (this.state.robots.length === 0) {
-      return <h1 className='tc'>Loading...</h1>
-    } else {
-      return (
+    return !robots.length ?
+      <h1 className='tc'>Loading...</h1> :
+      (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
@@ -39,7 +39,6 @@ class App extends React.Component {
           </Scroll>
         </div>
       );
-    }
   };
 };
 
